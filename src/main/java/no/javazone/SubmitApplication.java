13c30c059c155020b2e839690a9f4d2a2a1e4f13
Sveinung Dalatun <sdalatun@gmail.com>
2016-10-22 10:23:14 +0200

    Få opp dropwizard

---
+++ src/main/java/no/javazone/SubmitApplication.java
@@ -0,0 +1,19 @@
+package no.javazone;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+import no.javazone.api.ForslagResource;
+
+public class SubmitApplication extends Application<SubmitConfiguration> {
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0) {
+            args = new String[]{"server", "configuration.yaml"};
+        }
+        new SubmitApplication().run(args);
+    }
+
+    @Override
+    public void run(SubmitConfiguration submitConfiguration, Environment environment) throws Exception {
+        environment.jersey().register(new ForslagResource());
+    }
+}
---
+++ src/main/java/no/javazone/SubmitConfiguration.java
@@ -0,0 +1,6 @@
+package no.javazone;
+
+import io.dropwizard.Configuration;
+
+public class SubmitConfiguration extends Configuration {
+}
