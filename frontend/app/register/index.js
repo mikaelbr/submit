5ff16b49214b3fd173a66ab6b8676e05dcdbe2cd
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2016-11-01 21:06:55 +0100

    Kaller ressurs for å sende token på epost. Innført superagent. Null react-pattern foreløpig, I need the @bendiksolheim to the resque ;)

--- frontend/app/register/index.js
+++ frontend/app/register/index.js
@@ -1,11 +1,20 @@
 import r from 'r-dom';
+import request from 'superagent';
 
 const text = {
 };
 
 const submitEmail = () => {
-    window.location.hash = 'registerThanks';
-}
+    var emailAddress = document.getElementById('email-address').value;
+    request.get('http://localhost:8081/users/authtoken?email=' + emailAddress)
+        .end(function(err, res){
+            if (err || !res.ok) {
+                alert('Couldn\' send login link to ' + emailAddress + '. Please contact program@java.no to get help.');
+            } else {
+                window.location.hash = 'registerThanks';
+            }
+        });
+};
 
 const register = () => (
     r.div({className: 'welcome'}, [
@@ -14,7 +23,7 @@ const register = () => (
         ]),
         r.h1('Got something interesting to say?'),
         r.div({className: 'email-wrapper'}, [
-            r.input({type: 'email', className: 'email', placeholder: 'Your email address'}),
+            r.input({type: 'email', className: 'email', id: 'email-address', placeholder: 'Your email address'}),
             r.button({className: 'submit', type: 'submit', onClick: submitEmail})
         ]),
         r.div({className: 'explanation'}, [
