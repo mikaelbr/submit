5ac8f05fd82de0b600bd35bd3b5bc37373f39f72
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2016-11-01 21:05:37 +0100

    Legger på CORS header, så man kan kalle backenden fra frontend

--- backend/src/main/java/no/javazone/SubmitApplication.java
+++ backend/src/main/java/no/javazone/SubmitApplication.java
@@ -2,8 +2,11 @@ package no.javazone;
 
 import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
+import no.javazone.filters.CorsFilter;
 import no.javazone.resources.RootResource;
 import no.javazone.resources.UserResource;
+
+import java.util.List;
 
 import static java.util.Arrays.asList;
 
@@ -18,9 +21,20 @@ public class SubmitApplication extends Application<SubmitConfiguration> {
 
     @Override
     public void run(SubmitConfiguration configuration, Environment environment) throws Exception {
-        asList(
+        resources(configuration).forEach(resource -> environment.jersey().register(resource));
+        filters(configuration).forEach(filter -> environment.jersey().register(filter));
+    }
+
+    private List<Object> resources(SubmitConfiguration configuration) {
+        return asList(
                 new RootResource(),
                 new UserResource(configuration)
-        ).forEach(r -> environment.jersey().register(r));
+        );
+    }
+
+    private List<Object> filters(SubmitConfiguration configuration) {
+        return asList(
+                new CorsFilter()
+        );
     }
 }
---
+++ backend/src/main/java/no/javazone/filters/CorsFilter.java
@@ -0,0 +1,15 @@
+package no.javazone.filters;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import java.io.IOException;
+
+public class CorsFilter implements ContainerResponseFilter {
+
+	@Override
+	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+		// TODO (EHH): add correct URI
+		responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:8080");
+	}
+}
