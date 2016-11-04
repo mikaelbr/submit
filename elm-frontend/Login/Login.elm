bf6d483b536d079a1e49606d6c38c0657b99a41b
Bendik Solheim <bendik@solheim.im>
2016-11-04 13:11:13 +0100

    Restructure with elm-format

--- elm-frontend/Login/Login.elm
+++ elm-frontend/Login/Login.elm
@@ -5,45 +5,61 @@ import Html.App exposing (program)
 import Html.Attributes exposing (class, src, type', id, placeholder, value)
 import Html.Events exposing (onClick)
 
-type alias Model = { email: String }
 
-init  : (Model, Cmd Msg)
-init  = (initModel, Cmd.none)
+type alias Model =
+    { email : String }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initModel, Cmd.none )
+
 
 initModel : Model
-initModel = Model ""
+initModel =
+    Model ""
 
-type Msg = Update
-         | SubmitEmail
 
-update : Msg -> Model -> (Model, Cmd Msg)
+type Msg
+    = Update
+    | SubmitEmail
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Update -> (model, Cmd.none)
-        SubmitEmail -> (model, Cmd.none)
+        Update ->
+            ( model, Cmd.none )
+
+        SubmitEmail ->
+            ( model, Cmd.none )
+
 
 view : Model -> Html Msg
 view model =
     div [ class "welcome" ]
         [ div [ class "logo-wrapper" ]
-              [ img [ src "assets/logo.png", class "logo" ] [] ]
+            [ img [ src "assets/logo.png", class "logo" ] [] ]
         , h1 [] [ text "Got something interesting to say?" ]
         , div [ class "email-wrapper" ]
-              [ input [ value model.email, type' "email", class "email", id "email-address", placeholder "Your email address"] []
-              , button [ class "submit", type' "submit", onClick SubmitEmail] []
-              ]
+            [ input [ value model.email, type' "email", class "email", id "email-address", placeholder "Your email address" ] []
+            , button [ class "submit", type' "submit", onClick SubmitEmail ] []
+            ]
         , div [ class "explanation" ]
-              [ div [ class "arrow" ] []
-              , div [ class "text" ]
-                    [ div [] [ text "We’ll email you a unique login link."]
-                    , div [] [ text "Then you can start creating your talk." ]
-                    ]
-              ]
+            [ div [ class "arrow" ] []
+            , div [ class "text" ]
+                [ div [] [ text "We’ll email you a unique login link." ]
+                , div [] [ text "Then you can start creating your talk." ]
+                ]
+            ]
         ]
 
 
 subscriptions : Model -> Sub Msg
-subscriptions model = Sub.none
+subscriptions model =
+    Sub.none
+
 
 main : Program Never
-main = program { init = init, update = update, view = view, subscriptions = subscriptions}
+main =
+    program { init = init, update = update, view = view, subscriptions = subscriptions }
--- elm-frontend/Main.elm
+++ elm-frontend/Main.elm
@@ -1,27 +1,42 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.App exposing (program, map)
 import Html.Attributes exposing (class)
 import Login.Login as Login
 
-type alias Model = { login : Login.Model }
+
+type alias Model =
+    { login : Login.Model }
+
 
 initModel : Model
-initModel = Model (Login.initModel)
+initModel =
+    Model (Login.initModel)
 
-init  : (Model, Cmd Msg)
-init  = (initModel, Cmd.none)
 
-type Msg = Login Login.Msg
+init : ( Model, Cmd Msg )
+init =
+    ( initModel, Cmd.none )
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+type Msg
+    = Login Login.Msg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Login loginMsg ->
             let
-                (newLogin, loginCmd) = Login.update loginMsg model.login
-                mappedCmd = Cmd.map Login loginCmd
+                ( newLogin, loginCmd ) =
+                    Login.update loginMsg model.login
+
+                mappedCmd =
+                    Cmd.map Login loginCmd
             in
-                ({ model | login = newLogin}, mappedCmd)
+                ( { model | login = newLogin }, mappedCmd )
+
 
 view : Model -> Html Msg
 view model =
@@ -29,7 +44,10 @@ view model =
 
 
 subscriptions : Model -> Sub Msg
-subscriptions model = Sub.none
+subscriptions model =
+    Sub.none
+
 
 main : Program Never
-main = program { init = init, update = update, view = view, subscriptions = subscriptions}
+main =
+    program { init = init, update = update, view = view, subscriptions = subscriptions }
