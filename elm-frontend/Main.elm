cd84811d7fab54cdf1e8c2ed32a4a27bd6b36a24
Bendik Solheim <bendik@solheim.im>
2016-11-04 11:10:01 +0100

    Forgot to commit file. Oops.

--- elm-frontend/Main.elm
+++ elm-frontend/Main.elm
@@ -1,21 +1,31 @@
 import Html exposing (..)
-import Html.App exposing (program)
+import Html.App exposing (program, map)
+import Html.Attributes exposing (class)
+import Login.Login as Login
 
-type alias Model = String
+type alias Model = { login : Login.Model }
+
+initModel : Model
+initModel = Model (Login.initModel)
 
 init  : (Model, Cmd Msg)
-init  = ("Hello", Cmd.none)
+init  = (initModel, Cmd.none)
 
-type Msg = Update
+type Msg = Login Login.Msg
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
-        Update -> (model, Cmd.none)
+        Login loginMsg ->
+            let
+                (newLogin, loginCmd) = Login.update loginMsg model.login
+                mappedCmd = Cmd.map Login loginCmd
+            in
+                ({ model | login = newLogin}, mappedCmd)
 
 view : Model -> Html Msg
 view model =
-    div [] [ text model ]
+    div [ class "app" ] [ map Login (Login.view model.login) ]
 
 
 subscriptions : Model -> Sub Msg
