aa18e10077ff2fe88c81061a5b44886c481e0d22
Bendik Solheim <bendik@solheim.im>
2016-12-06 19:59:02 +0100

    Try to show submissions

--- frontend/src/Submissions/View.elm
+++ frontend/src/Submissions/View.elm
@@ -2,9 +2,35 @@ module Submissions.View exposing (view)
 
 import Submissions.Model exposing (..)
 import Submissions.Messages exposing (..)
-import Html exposing (Html, div, text)
+import Html exposing (Html, div, h1, h2, text)
+import Html.Attributes exposing (class)
 
 
 view : Model -> Html Msg
 view model =
-    div [] [ text "Submissions" ]
+    let
+        years =
+            List.map viewYear model.years
+    in
+        div []
+            [ h1 [] [ text "Your Talks" ]
+            , div [] years
+            ]
+
+
+viewYear : Year -> Html Msg
+viewYear year =
+    let
+        submissions =
+            List.map viewSubmission year.submissions
+    in
+        div [ class "submissions__year" ]
+            [ h2 [] [ text <| toString year.year ]
+            , div [] submissions
+            ]
+
+
+viewSubmission : Submission -> Html Msg
+viewSubmission submission =
+    div [ class "submissions__submission" ]
+        [ text submission.name ]
