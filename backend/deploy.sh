53ec9e78a29b66897303ce17058ced061325a11b
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2017-01-08 20:24:09 +0100

    Deployment script supporting secret properties. Also added support for environment specific properties

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,13 +1,40 @@
-#/bin/bash
+#!/usr/bin/env bash
 
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
 
-echo -e "${GREEN}Fetching your active Elastic Beanstalk environments...${NC}"
-eb list --profile javabin
-echo ""
-echo -e "${GREEN}Type name of the environment you want to deploy to:${NC}"
-read ENVIRONMENT
-echo -e "${GREEN}Deploying backend to $ENVIRONMENT${NC}"
+BASEDIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
-eb deploy "$ENVIRONMENT"  --profile javabin
+# resolve symlinks
+while [ -h "$BASEDIR/$0" ]; do
+    DIR=$(dirname -- "$BASEDIR/$0")
+    SYM=$(readlink $BASEDIR/$0)
+    BASEDIR=$(cd $DIR && cd $(dirname -- "$SYM") && pwd)
+done
+cd ${BASEDIR}
+
+ENVS=$(eb list --profile javabin | sed 's/^\* //')
+
+if [[ ${1} != submit-* ]]; then
+  echo "Usage: ${0} submit-<environment>"
+  exit 1
+elif [ $(echo "$ENVS" | grep "^$1$" -c) -eq 0 ]; then
+  echo "Environment not recognized: '$1'. Use one of the following:"
+  echo
+  echo "$ENVS"
+  exit 1
+fi
+
+BEANSTALK_ENV=$1
+ENV="$(echo $BEANSTALK_ENV | cut -d '-' -f 2)"
+
+echo "> preparing secret properties"
+
+ansible-vault view "secretconfig/application-$ENV.properties.encrypted" >> "application-$ENV.properties"
+
+git add "application-$ENV.properties"
+
+echo "> Starting deploy"
+eb deploy "$BEANSTALK_ENV" --staged --profile javabin
+
+git rm -f "application-$ENV.properties"
+
+echo "Deploy complete."
