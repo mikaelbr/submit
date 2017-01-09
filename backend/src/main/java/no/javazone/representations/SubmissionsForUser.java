c9d1cb41ab70ac4fd0ac9119c2a344c116a49925
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2017-01-09 23:18:25 +0100

    Removing mess… :P

--- backend/src/main/java/no/javazone/representations/SubmissionsForUser.java
+++ backend/src/main/java/no/javazone/representations/SubmissionsForUser.java
@@ -2,7 +2,6 @@ package no.javazone.representations;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class SubmissionsForUser {
 
@@ -12,15 +11,4 @@ public class SubmissionsForUser {
         this.years = years;
     }
 
-    public void addForYear(String year, Submission submission) {
-        // TODO (EHH): refactor this mess!
-        Optional<Year> foundYear = years.stream().filter(y -> y.year.equals(year)).findFirst();
-        if(foundYear.isPresent()) {
-            foundYear.get().addSubmission(submission);
-        } else {
-            Year newYear = new Year(year);
-            years.add(newYear);
-            newYear.addSubmission(submission);
-        }
-    }
 }
