391f3a288f3ee9d8e895af93828d3531290288a5
Bendik Solheim <bendik@solheim.im>
2017-01-11 17:32:55 +0100

    Submission json encoder

--- frontend/src/Submission/Encoder.elm
+++ frontend/src/Submission/Encoder.elm
@@ -1,10 +1,29 @@
 module Submission.Encoder exposing (encoder)
 
 import Submission.Model exposing (..)
-import Json.Encode exposing (Value, object, string)
+import Json.Encode exposing (Value, object, string, list)
 
 
 encoder : Submission -> Value
 encoder submission =
     object
-        [ ( "abstract", string submission.abstract ) ]
+        [ ( "status", string submission.status )
+        , ( "title", string submission.title )
+        , ( "abstract", string submission.abstract )
+        , ( "intendedAudience", string submission.intendedAudience )
+        , ( "format", string submission.format )
+        , ( "language", string submission.language )
+        , ( "keywords", list <| List.map string submission.keywords )
+        , ( "outline", string submission.outline )
+        , ( "speakers", list <| List.map encodeSpeaker submission.speakers )
+        ]
+
+
+encodeSpeaker : Speaker -> Value
+encodeSpeaker speaker =
+    object
+        [ ( "id", string speaker.id )
+        , ( "name", string speaker.name )
+        , ( "email", string speaker.email )
+        , ( "bio", string speaker.bio )
+        ]
