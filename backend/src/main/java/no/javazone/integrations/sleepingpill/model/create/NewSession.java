fe461706d4c39fdab5b4b61c6172c0ac245cdb00
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2017-01-11 16:57:08 +0100

    Remove unused code

--- backend/src/main/java/no/javazone/integrations/sleepingpill/model/create/NewSession.java
+++ backend/src/main/java/no/javazone/integrations/sleepingpill/model/create/NewSession.java
@@ -34,38 +34,6 @@ public class NewSession {
         data.setOutline(outline);
     }
 
-    public String getTitle() {
-        return data.title != null ? data.title.value : null;
-    }
-
-    public String getAbstract() {
-        return data.theAbstract != null ? data.theAbstract.value : null;
-    }
-
-    public String getIntendedAudience() {
-        return data.intendedAudience != null ? data.intendedAudience.value : null;
-    }
-
-    public String getFormat() {
-        return data.format != null ? data.format.value : null;
-    }
-
-    public String getLanguage() {
-        return data.language != null ? data.language.value : null;
-    }
-
-    public String getPublished() {
-        return data.published != null ? data.published.value : null;
-    }
-
-    public List<String> getKeywords() {
-        return data.keywords != null ? data.keywords.value : null;
-    }
-
-    public String getOutline() {
-        return data.outline != null ? data.outline.value : null;
-    }
-
     public static NewSession draft(EmailAddress postedBy) {
         return new NewSession(
                 SessionStatus.DRAFT,
