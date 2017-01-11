01a368dce7d27b61fba30d29bbd007329bfb733f
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2017-01-11 17:20:56 +0100

    More methods

--- backend/src/main/java/no/javazone/filters/CORSFilter.java
+++ backend/src/main/java/no/javazone/filters/CORSFilter.java
@@ -22,7 +22,7 @@ public class CORSFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "GET");
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,X-token,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
