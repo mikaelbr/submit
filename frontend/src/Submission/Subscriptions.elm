a936ac4610374fcd924db577da16a44b9978ecab
Bendik Solheim <bendik@solheim.im>
2017-02-06 22:40:42 +0100

    Never autosave historic talks

--- frontend/src/Submission/Subscriptions.elm
+++ frontend/src/Submission/Subscriptions.elm
@@ -3,11 +3,17 @@ module Submission.Subscriptions exposing (subscriptions)
 import Submission.Model exposing (..)
 import Submission.Messages exposing (..)
 import Time exposing (every, second)
+import Backend.Network exposing (RequestStatus(..))
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    if model.autosave && model.dirty then
-        every (30 * second) Save
-    else
-        Sub.none
+    case model.submission of
+        Complete submission ->
+            if model.autosave && model.dirty && submission.status /= "HISTORIC" then
+                every (30 * second) Save
+            else
+                Sub.none
+
+        _ ->
+            Sub.none
