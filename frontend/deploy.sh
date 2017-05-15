daa9c541f794edbf4ed83f6187b089efe532454d
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2017-05-15 11:25:02 +0200

    Fix frontend deploy

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -9,7 +9,7 @@ NC='\033[0m' # No Color
 REGION="eu-central-1"
 
 echo -e "${GREEN}Fetching your active buckets in region $REGION...${NC}"
-aws s3 ls --profile javabin | grep submit
+aws s3 ls --profile bekkfagdag | grep submit
 echo ""
 echo -e "${GREEN}Type bucket name where you want to deploy frontend:${NC}"
 read BUCKET_NAME
@@ -17,4 +17,4 @@ echo -e "${GREEN}Deploying frontend to $BUCKET_NAME in $REGION${NC}"
 
 npm run build
 
-aws s3 sync ./dist/ "s3://$BUCKET_NAME" --region="$REGION" --profile javabin --delete
+aws s3 sync ./dist/ "s3://$BUCKET_NAME" --region="$REGION" --profile bekkfagdag --delete
